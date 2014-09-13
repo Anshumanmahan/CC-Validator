edae5115f58a5c26bc450e03755671ded0194f06
Anshuman Dixit <anshumandixit1996@gmail.com>
2014-09-13 15:41:22 +0530

    Modified

--- cc_validator.cpp
+++ cc_validator.cpp
@@ -4,6 +4,7 @@
 #include <string>
 using namespace std;
 
+const int MAX_CREDIT_CARD_SIZE = 16;
 int digitize (int x) {
   if (x < 10) {
     return x;
@@ -12,14 +13,26 @@ int digitize (int x) {
   }
 }
 
-int main(int argc, char** argv) {
-  if (argc != 2) {
-    printf("Usage:\n./cc_validator <CREDIT_CARD_NUMBER>\n");
-    return 0;
+int Length(char* ptr) {
+  int res = 0;
+  char* newptr = ptr;
+  while(*newptr != '\0') {
+	newptr++;
+	res++;
   }
-  string card = argv[1];
-  int length = card.length();
-  int checksum = 0;
+  return res;
+}
+
+int main(int argc, char** argv) {
+  char* card = (char*)malloc(sizeof(char) * MAX_CREDIT_CARD_SIZE); 
+  if (argc == 1) {
+    printf("Enter Credit Card Number: ");
+    scanf("%s",card);   
+  } else {
+   card = argv[1]; 
+  }
+  int length = Length(card);
+  int checksum = 0 ;
   bool flag = false;
   for (int i = length - 1; i >= 0; i--) {
     if (flag == false) {
@@ -32,9 +45,9 @@ int main(int argc, char** argv) {
     }
   }
   if (checksum % 10 == 0) {
-    printf("Valid Credit Card");
+    printf("Valid Credit Card\n");
   } else {
-    printf("Invalid Credit Card!!");
+    printf("Invalid Credit Card!!\n");
   }
   return 0;
 }
